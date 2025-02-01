ec91dbf771616a859d74bafcb11bf79cbf5bdc7b
vegaronel <ronelvega31@gmail.com>
2025-02-01 14:41:53 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express();
+
+app.get("/", (req, res)=> {
+    res.send("Backend is Working");
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is running");
+})
